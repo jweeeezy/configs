c8928bebcdb42a441cd28d5b17f3bad38e3b2f11
jweeeezy <willertjakob@gmail.com>
2025-02-04 19:49:45 +0100

    [tools/] add remove_credentials_from_bash_history.sh
    - uses list of keywords
    - shows user lines in .bash_history containing those keywords
    - prompts wether these lines should be deleted

---
+++ tools/bash/remove_credentials_from_bash_history.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# History File Path
+HISTORY_FILE="~/.bash_history"
+
+
+# Helper Function
+# Hint: Add or remove keywords to search for as you like
+function has_credential() {
+    local line="$1"
+    local keywords=("password" "api" "key" "pw")
+
+    line="${line,,}"
+
+    for keyword in "${keywords[@]}"; do
+        if [[ "$line" =~ $keyword ]]; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+
+# Step 1: Read History File
+# Read line per line from HISTORY_FILE
+# if credential found:
+#   save line into buffer with credentials
+# if no:
+#   save line into clean buffer
+while IFS= read -r line
+do
+    if has_credential "$line"; then
+        echo "Line with Credential found:"
+        BUFFER_CREDENTIALS+="$line"$'\n'
+    else
+        BUFFER_CLEAN+="$line"$'\n'
+    fi
+done < $(eval echo "$HISTORY_FILE")
+
+
+# Step 2: User Prompt with Results
+# Show User lines with credentials found and prompt wether deletion is wanted
+echo "$BUFFER_CREDENTIALS"
+read -p "Do you want to remove these lines from your history? (y/N): " choice
+choice="${choice,,}"
+
+
+# Step 3: Execute Users Choice
+# On accept:
+#   Use Clean_Buffer to overwrite HISTORY_FILE
+# On decline:
+#   Do Nothing
+if [[ "$choice" == "y" || "$choice" == "yes" ]]; then
+    echo -n "$BUFFER_CLEAN" > $(eval echo "$HISTORY_FILE")
+    echo "Sensitive lines removed from history."
+else
+    echo "No changes made."
+fi
