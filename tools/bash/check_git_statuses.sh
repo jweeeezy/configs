18ac9b791c31cfdd3fc34a55ab3d54228bd127d9
jweeeezy <willertjakob@gmail.com>
2025-10-28 09:37:41 +0100

    [tools/] [#38] add unpushed status to check_git_statuses.sh

--- tools/bash/check_git_statuses.sh
+++ tools/bash/check_git_statuses.sh
@@ -2,6 +2,7 @@
 
 GREEN="\033[92m"
 RED="\033[91m"
+YELLOW="\033[93m"
 RESET="\033[0m"
 
 # Check if a git repository is clean (no changes)
@@ -16,16 +17,21 @@ is_clean_git_repo() {
 }
 
 # Check if a git repository has unpushed commits
-# Return 0 if none, return 1 if yes
+# Return 0 if yes, return 1 if no
 has_unpushed_commits() {
     local repo_dir="$1"
-    if git -C "$repo_dir" log \
-        --branches --not --remotes --oneline -1 >/dev/null
-    then
+
+    # Check if branch has upstream
+    if ! git -C "$repo_dir" rev-parse --abbrev-ref '@{u}' >/dev/null 2>&1; then
         return 0
-    else
-        return 1
     fi
+
+    # Check for commits ahead of upstream
+    if [[ -n $(git -C "$repo_dir" log '@{u}'..HEAD --oneline) ]]; then
+        return 0
+    fi
+
+    return 1
 }
 
 # List the given directory if it's a git repo and report its status
@@ -45,10 +51,15 @@ list_git_repos_with_status() {
     is_clean_git_repo "$dir"
     local is_clean=$?
 
-    if [[ $is_clean -eq 0 ]]; then
+    has_unpushed_commits "$dir"
+    local has_unpushed=$?
+
+    if [[ $is_clean -eq 0 && $has_unpushed -eq 1 ]]; then
         [[ $show_dirty_only -eq 1 ]] && return
         local status="clean"
         local color="${GREEN}"
+    elif [[ $has_unpushed -eq 0 ]]; then
+        local status="unpushed"; color="${YELLOW}"
     else
         local status="dirty"
         local color="${RED}"
@@ -77,14 +88,14 @@ Usage: $0 [options] <dir1> [dir2 ... dirN]
 Recursively scan given directory/ies for Git repositories.
 
 Options:
-  -t      Show 'clean' or 'dirty' git status next to each repo.
-  -d      Show only dirty repositories (hide clean ones).
-  -c      Use colored output (green for clean, red for dirty).
+  -t      Show 'clean', 'dirty' or 'unpushed' git status next to each repo.
+  -d      Show only 'dirty' and 'unpushed' repositories (hide clean ones).
+  -c      Use colored output (green for clean, red for dirty, yellow for unpushed).
   -h      Show this help message and exit.
 
 Examples:
-  $0 -t ~/projects       # Show status for all repos under ~/projects
-  $0 -dc ~/code ~/work   # Show only dirty repos in color under both directories
+  $0 -t ~/projects       # Show status for all repos in ~/projects
+  $0 -dc ~/code ~/work   # Show only dirty and unpushed repos in color in ~/code and ~/work
 EOF
 }
 
