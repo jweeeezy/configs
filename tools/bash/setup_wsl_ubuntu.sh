877d99a6cfdc3defc79ee98a66262f622650f23b
jweeeezy <willertjakob@gmail.com>
2026-02-25 18:43:17 +0100

    work in progress: ubuntu setup / dockerfile

--- tools/bash/setup_wsl_ubuntu.sh
+++ tools/bash/setup_wsl_ubuntu.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
+export DEBIAN_FRONTEND=noninteractive
+export TZ=Europe/Berlin
+
+LOG_PREFIX="Setup Ubuntu Script |"
+
+export SUDO="sudo"
+if [ -f "/.dockerenv" ] || [ "${CONTAINER:-}" = "true" ]; then
+    export SUDO=""
+fi
+
 # Update system packages
-echo "Updating System Packages..."
+echo "$LOG_PREFIX Updating System Packages..."
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 # Install necessary Linux Tools
-echo "Installing Linux Tools..."
+echo "$LOG_PREFIX Installing Linux Tools..."
 sudo apt-get install -y \
     git \
     make \
@@ -13,34 +23,38 @@ sudo apt-get install -y \
     direnv \
     ack \
     python3-pip \
-    golang-go
+    golang-go \
+    curl
 
 # Install Configs Repository (Dotfiles, Bash Scripts)
-echo "Installing Config Repository including .dotfiles and Bash scripts..."
+echo "$LOG_PREFIX Installing Config Repository including .dotfiles and Bash scripts..."
 git clone https://github.com/jweeeezy/configs.git ~/configs \
     && make install -C ~/configs
 # shellcheck source=dotfiles/.bashrc
 source ~/.bashrc || true
 
 # Install fzf and add to .bashrc
-echo "Installing Fuzzy Find..."
+echo "$LOG_PREFIX Installing Fuzzy Find..."
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf &&
     ~/.fzf/install --all
 
 # Install Vim PLugins
-echo "Installing Vim Plugin Folder..."
+echo "$LOG_PREFIX Installing Vim Plugin Folder..."
 mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle \
     && git clone https://github.com/VundleVim/Vundle.vim.git Vundle.vim
 
 # Generate ssh Key for Github
-echo "Generating SSH Key for Github Authorization..."
+echo "$LOG_PREFIX Generating SSH Key for Github Authorization..."
 mkdir -p ~/.ssh/github/
-read -rp "Enter your Github Mail address: " mail_address
-ssh-keygen -t rsa -b 4096 -C "$mail_address" -f ~/.ssh/github
+
+if [[ -z "${GITHUB_MAIL:-}" ]]; then
+    read -rp "$LOG_PREFIX Enter your Github Mail address: " mail_address
+    ssh-keygen -t rsa -b 4096 -C "$mail_address" -f ~/.ssh/github/id.rsa
+fi
 
 # Install Node Version Manager
-echo "Installing Node Version Manager..."
+echo "$LOG_PREFIX Installing Node Version Manager..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 # shellcheck disable=SC1091
@@ -50,17 +64,17 @@ export NVM_DIR="$HOME/.nvm"
 nvm install --lts
 
 # Install pipx
-echo "Installing Pipx..."
+echo "$LOG_PREFIX Installing Pipx..."
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
 # shellcheck source=dotfiles/.bashrc
 source ~/.bashrc || true
 
 # Install Ruff + Pylsp
-echo "Installing Pylsp + Ruff Plugin ..."
+echo "$LOG_PREFIX Installing Pylsp + Ruff Plugin ..."
 pipx install python-lsp-server
 pipx inject python-lsp-server python-lsp-ruff
 
 # Install bash language server
-echo "Installing Bash Language Server ..."
+echo "$LOG_PREFIX Installing Bash Language Server ..."
 npm i -g bash-language-server
